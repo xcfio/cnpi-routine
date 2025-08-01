074e81b1ba1bea882b4a43303cdf2d63b94ebe1d
xcfio <119097812+xcfio@users.noreply.github.com>
2025-08-01 22:38:49 +0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,18 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
     title: "Chapainawabganj Polytechnic Institute - Class Routine",
-    description: "Academic Schedule Management System for Chapainawabganj Polytechnic Institute"
+    description: "Academic Schedule for Chapainawabganj Polytechnic Institute",
+    keywords: 'class routine, polytechnic, 2025, Chapainawabganj',
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
+                <title>Chapainawabganj Polytechnic Class Routine</title>
+                <meta name="description" content="Academic Schedule for Chapainawabganj Polytechnic Institute" />
+                <meta name="keywords" content="class routine, polytechnic, 2025, Chapainawabganj" />
+                <meta name="theme-color" content="#2196f3">
                 <meta name="google-site-verification" content="etSkyVopRchIy48MQQWDkKZ3lKmJAKnLpa690GsAXcU" />
             </head>
             <body>{children}</body>
